32ef80b643ce613b506fb3fa2bd8f70a11acf980
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-08-07 19:19:08 -0300

    criei um relógio usando setInterval

--- logicaProgramacao/timer/assets/main.js
+++ logicaProgramacao/timer/assets/main.js
@@ -1,15 +1,43 @@
-const cronometro = document.querySelector('.cronometro')
-const iniciar = document.querySelector('#btn_iniciar')
-const pausar = document.querySelector('#btn_pausar')
-const zerar = document.querySelector('#btn_zerar')
+function relogio() {
+function getTimeFromSeconds(segundos) {
+  const data = new Date(segundos * 1000)
+  return data.toLocaleTimeString('pt-BR', { hour12: false, timeZone: 'GMT' })
+}
 
-iniciar.addEventListener('click', function(event) {
+const relogio = document.querySelector('.relogio')
+const iniciar = document.querySelector('.iniciar')
+const pausar = document.querySelector('.pausar')
+const zerar = document.querySelector('.zerar')
+let segundos = 0
+let timer
+
+function iniciaRelogio() {
+  timer = setInterval(function () {
+    segundos++
+    relogio.innerHTML = getTimeFromSeconds(segundos)
+  }, 1000)
+}
+
+document.addEventListener('click', function (e) {
+  const el = e.target
+
+  if (el.classList.contains('iniciar')) {
+    clearInterval(timer)
+    relogio.classList.remove('pausado')
+    iniciaRelogio()
+  }
+
+  if (el.classList.contains('pausar')) {
+    clearInterval(timer)
+    relogio.classList.add('pausado')
+  }
+
+  if (el.classList.contains('zerar')) {
+    clearInterval(timer)
+    relogio.classList.remove('pausado')
+    relogio.innerHTML = '00:00:00'
+    segundos = 0
+  }
 })
-
-pausar.addEventListener('click', function(event) {
-  
-})
-
-zerar.addEventListener('click', function(event) {
-
-})
+}
+relogio()
