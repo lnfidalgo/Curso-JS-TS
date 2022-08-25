6bc44130aed28424ca8569633e237db996d636e6
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-08-25 00:08:25 -0300

    Update main.js

--- logicaProgramacao/calculadora/assets/main.js
+++ logicaProgramacao/calculadora/assets/main.js
@@ -4,26 +4,26 @@ function criaCalculadora() {
 
     calculadora() {
       this.iniciaCalculadora()
-      this.contaEnter()
     },
 
     iniciaCalculadora() {
       document.addEventListener('click', e => {
         const el = e.target
+
         if (el.classList.contains('btn_num')) {
           this.btnNum(el.innerText)
-        }
-
-        if (el.classList.contains('btn_del')) {
-          this.btnDel()
         }
 
         if (el.classList.contains('btn_clear')) {
           this.btnClear()
         }
 
+        if (el.classList.contains('btn_del')) {
+          this.btnDel()
+        }
+
         if (el.classList.contains('btn_eq')) {
-          this.criaResultado()
+          this.btnEq()
         }
 
         this.display.focus()
@@ -34,28 +34,20 @@ function criaCalculadora() {
       this.display.value += valor
     },
 
-    btnDel() {
-      this.display.value = this.display.value.slice(0, -1)
-    },
-
     btnClear() {
       this.display.value = ''
     },
 
-    contaEnter() {
-      this.display.addEventListener('keyup', e => {
-        if (e.keyCode === 13) {
-          this.criaResultado()
-        }
-      })
+    btnDel() {
+      this.display.value = this.display.value.slice(0, -1)
     },
 
-    criaResultado() {
+    btnEq() {
       let conta = this.display.value
       try {
         conta = eval(conta)
-        if (!conta) {
-          alert('Conta inválida')
+        if(!conta) {
+          alert('Conta inválida!')
           return
         }
         this.display.value = conta
@@ -65,5 +57,6 @@ function criaCalculadora() {
     },
   }
 }
+
 const calculadora = criaCalculadora()
 calculadora.calculadora()
