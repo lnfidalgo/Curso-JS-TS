4f6666c95631964856991cff9d19b0136d865e95
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-08-25 21:16:42 -0300

    Aprendendo Flex-box

---
+++ array/splice.js
@@ -0,0 +1,7 @@
+const nomes = ['Lucas', 'Pedro', 'Davi', 'Napoleão', 'Matheus', 'Jonas', 'Dedeh']
+
+// nome.splice(indice, delete, elemento)
+
+// pop
+const removidos = nomes.splice(6, 1)
+console.log(nomes, removidos)
