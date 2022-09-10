524c3e3e498ecd548903f401880d6614bd77fc56
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-09-09 23:56:49 -0300

    estudando import e export

---
+++ webPackEmDiante/importExport/src/index.js
@@ -0,0 +1,4 @@
+import { nome as nome2, sobrenome, idade, soma } from "./modulo1";
+const nome = 'Aipim'
+console.log(nome, nome2, sobrenome, idade)
+console.log(soma(5, 4))
---
+++ webPackEmDiante/importExport/src/modulo1.js
@@ -0,0 +1,10 @@
+export { nome, sobrenome, idade, soma }
+
+
+const nome = 'Lucas'
+const sobrenome = 'Fidalgo'
+const idade = 21
+
+function soma(x, y) {
+  return x + y
+} 
