b514b8e93571ad50384d76273da009ae4c36a41a
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-09-14 14:01:51 -0300

    .

--- aulasAtéPromises/testes/main.js
+++ aulasAtéPromises/testes/main.js
@@ -1,6 +1,47 @@
-function testanto() {
-  const numeros = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  const random = Math.floor(Math.random() * numeros.length)
-  return random
+const form = document.querySelector('#formulario')
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  const inputC = event.target.querySelector('#celcius')
+  const inputF = event.target.querySelector('#fare')
+  const celcius = Number(inputC.value)
+  const fare = Number(inputF.value)
+
+  if (!celcius) {
+    setResultado('Valor inválido')
+    return
+  }
+
+  if (!fare) {
+    setResultado('Valor inválido')
+    return
+  }
+
+  const tempC = getC(celcius, fare)
+  const tempF = getF(fare, celcius)
+  const msg = `Aqui o celcius ${tempC}ºC. Aqui o fare ${tempF}ºF`
+
+  setResultado(msg)
+})
+
+function getC(c, f) {
+  return c + f
 }
-console.log(testanto())
+
+function getF(f, c) {
+  return f * c
+}
+
+function criaP() {
+  const p = document.createElement('p')
+  return p
+}
+
+function setResultado(qualquerNome) {
+  const resultado = document.querySelector('.resultado')
+  resultado.innerHTML = ''
+
+  const p = criaP()
+  p.innerHTML = qualquerNome
+  p.classList.add('resultado-verde')
+  resultado.appendChild(p)
+}
