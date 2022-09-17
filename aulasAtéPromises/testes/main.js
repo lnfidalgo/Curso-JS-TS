bf755b0aceb4c73d887afada713a1ed1149832f8
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-09-17 14:44:07 -0300

    .

--- aulasAtéPromises/testes/main.js
+++ aulasAtéPromises/testes/main.js
@@ -1,47 +1,5 @@
-const form = document.querySelector('#formulario')
-form.addEventListener('submit', event => {
-  event.preventDefault()
-  const inputC = event.target.querySelector('#celcius')
-  const inputF = event.target.querySelector('#fare')
-  const celcius = Number(inputC.value)
-  const fare = Number(inputF.value)
-
-  if (!celcius) {
-    setResultado('Valor inválido')
-    return
-  }
-
-  if (!fare) {
-    setResultado('Valor inválido')
-    return
-  }
-
-  const tempC = getC(celcius, fare)
-  const tempF = getF(fare, celcius)
-  const msg = `Aqui o celcius ${tempC}ºC. Aqui o fare ${tempF}ºF`
-
-  setResultado(msg)
-})
-
-function getC(c, f) {
-  return c + f
-}
-
-function getF(f, c) {
-  return f * c
-}
-
-function criaP() {
-  const p = document.createElement('p')
-  return p
-}
-
-function setResultado(qualquerNome) {
-  const resultado = document.querySelector('.resultado')
-  resultado.innerHTML = ''
-
-  const p = criaP()
-  p.innerHTML = qualquerNome
-  p.classList.add('resultado-verde')
-  resultado.appendChild(p)
-}
+let cont = 1;
+do{
+cont += 1;
+}while (cont < 10);
+console.log(cont);
