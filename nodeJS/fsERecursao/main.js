e262b91ade5fd91a338ccb5d3b8993e8750934bd
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-09-29 23:29:53 -0300

    .

--- nodeJS/fsERecursao/main.js
+++ nodeJS/fsERecursao/main.js
@@ -10,14 +10,18 @@ async function readDir(rootDir) {
 
 async function walk(files, rootDir) {
   for (let file of files) {
-
     const fileFullPath = path.resolve(rootDir, file)
     const stats = await fs.stat(fileFullPath)
+
+    if (/\.git/g.test(fileFullPath)) continue
+    if (/node_modules/g.test(fileFullPath)) continue
 
     if (stats.isDirectory()) {
       readDir(fileFullPath)
       continue
     }
+
+    if (!/\.css$/g.test(fileFullPath)) continue // Movi para cá pois se não encontrasse na primeira, não iria continuar a resursão
 
     console.log(fileFullPath)
   }
