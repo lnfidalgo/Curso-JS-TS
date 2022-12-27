896f2c6e95277766c9479e6f50c131cea0929880
Lucas Fidalgo <lucasfidalgo07@gmail.com>
2022-12-27 19:00:00 -0300

    Atualizando conhecimentos sobre React-router-dom@6

---
+++ react_router_dom/tutorial-react-router-dom/src/components/Post/index.tsx
@@ -0,0 +1,12 @@
+export const Post = () => {
+  return (
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt eos
+        aut quisquam sapiente perferendis labore incidunt magni esse numquam
+        quasi vero quo harum doloremque odio, repellat, officia nesciunt, ut
+        enim?
+      </p>
+    </div>
+  );
+};
