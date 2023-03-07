304fea6a83567fe71111f6c7134b7f2ad6694ada
lnfidalgo <lucasfidalgo07@gmail.com>
2023-03-06 23:55:54 -0300

    class practice

--- aulas-type-script/aulas/class/class-test/src/game.ts
+++ aulas-type-script/aulas/class/class-test/src/game.ts
@@ -1,3 +1,31 @@
-export class Character {
-  constructor() {}
+export abstract class Character {
+  constructor(protected name: string, protected atack: number, protected life: number) {}
+
+  receiveAtack(oponent: Character): void {
+    this.bordao()
+    oponent.loseLife(this.atack)
+  }
+
+  protected loseLife(atackPower: number): void {
+    this.life-=atackPower
+    console.log(`${this.name} is losing life, ${this.life} life points`)
+  }
+
+  abstract bordao(): void
 }
+
+export class Mage extends Character {
+  bordao(): void {
+    console.log('SHAZAN')
+  }
+}
+export class Monster extends Character {
+  bordao(): void {
+    console.log('Ahhhh hurts...')
+  }
+}
+
+const wizard=new Mage('fimage', 100, 1000)
+const monstrin=new Mage('cruscredo', 80, 1000)
+
+wizard.receiveAtack(monstrin)
